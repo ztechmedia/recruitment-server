6b57ffcbc2dde947fb890bff26d225ebf52aa827
septian.arman009 <septian.arman009@gmail.com>
2020-08-14 06:00:42 +0700

    Recruitment Server #2

--- server.js
+++ server.js
@@ -78,6 +78,9 @@ app.use("/api/v1/jobs", jobs);
 
 app.use(errorHandler);
 
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
